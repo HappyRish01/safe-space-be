ce016e841ac33339725a5d80b963eb7be13cb7c2
Rish <rishk219@gmail.com>
2025-02-22 18:45:12 +0530

    testing ci/cd

--- src/index.js
+++ src/index.js
@@ -21,6 +21,11 @@ app.get("/", (req,res) => {
 // Routes file
 import routes from '../routes/index.js'
 app.use("/api", routes)
+app.use("/api/test", (req,res) => {
+    return res.status(200).json({
+        message: "everything working fine need to work on prisma thing"
+    })
+})
 //http:localhost:8080/user
 
 app.listen(PORT, ()=>{
