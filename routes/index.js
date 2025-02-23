5b9dd53b260ae4ed69f42e5e6813c9e641915fe4
Rish <rishk219@gmail.com>
2025-02-23 18:33:53 +0530

    handle media

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import postRoutes from "../routes/v1/post/index.js";
 import feedRoutes from "../routes/v1/post/feed/index.js";
 import communityRoutes from "../routes/v1/community/index.js";
 import {getPostsController} from "../controller/postController.js";
+import {generateUploadSignature,saveImage} from "../controller/mediaController.js";
 
 
 
@@ -17,6 +18,8 @@ router.use("/v1/post" ,checkForAuthentication, postRoutes);
 router.use("/v1/posts" ,checkForAuthentication, getPostsController);
 router.use("/v1/feeds" , feedRoutes);
 router.use("/v1/community",checkForAuthentication,communityRoutes)
+router.post("/v1/generate-upload-signature",checkForAuthentication,generateUploadSignature)
+router.post("/v1/save-image",checkForAuthentication,saveImage)
 
 
 export default router;
