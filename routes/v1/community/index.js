550e319d6788d81b42cc9367c5f5d136662d5198
Rish <rishk219@gmail.com>
2025-02-24 00:53:02 +0530

    communities feature added_tested

--- routes/v1/community/index.js
+++ routes/v1/community/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { prisma } from "../../../libs/prisma.js";
 
 const router = Router();
 
@@ -15,13 +16,13 @@ router.get("/communities", async (req, res) => {
 // Get all posts of a community
 router.get("/:id/posts", async (req, res) => {
   const { id } = req.params;
-  const {skip=0,take=15} = req.query;
+  const { skip = 0, take = 15 } = req.query;
 
   const userId = req.user?.id;
   try {
     const posts = await prisma.post.findMany({
-        skip,
-        take,
+      skip,
+      take,
       where: {
         communityId: id,
       },
@@ -42,20 +43,22 @@ router.get("/:id/posts", async (req, res) => {
         },
       },
       orderBy: {
-        createdAt: 'desc', // Sorting baby 
+        createdAt: "desc", // Sorting baby
       },
     });
     if (!posts) {
       return res.status(404).json({ error: "Post not found" });
     }
 
-    res.status(200).json( posts.map((post) => {
+    res.status(200).json(
+      posts.map((post) => {
         const { likes, ...rest } = post; // Destructure `likes` and keep the rest of the post data
         return {
           ...rest,
           isLiked: likes.length > 0, // Add `isLiked` flag
         };
-    }));
+      })
+    );
 
     //   res.status(200).json(posts);
   } catch (e) {
@@ -63,33 +66,135 @@ router.get("/:id/posts", async (req, res) => {
   }
 });
 
-router.post("/post",async(req,res) => {
-    const userId = req.user.id;
-    const {communityId,content ,caption} = req.body;
+router.post("/post", async (req, res) => {
+  const userId = req.user.id;
+  const { communityId, content, caption } = req.body;
 
-    try {
-        const post = await prisma.post.create({
-            data: {
-                content,
-                caption,
-                userId,
-                communityId,
-            }
-        })
+  try {
+    const post = await prisma.post.create({
+      data: {
+        content,
+        caption,
+        userId,
+        communityId,
+      },
+    });
 
-        res.status(200).json({
-            message: "Post created successfully",
-            post,
-            
-        })
-    }catch(e){
-        return res.status(500).json({
-            message: "Failed to create post"
-        })
+    res.status(200).json({
+      message: "Post created successfully",
+      post,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Failed to create post",
+    });
+  }
+});
+
+// join a community
+router.post("/:communityId/join", async (req, res) => {
+  const userId = req.user.id;
+
+  const { communityId } = req.params;
+
+  try {
+    //already joined a community
+    const exsistingCommunity = await prisma.community.findFirst({
+      where: {
+        id: communityId,
+        members: {
+          some: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    if (exsistingCommunity) {
+      return res.status(400).json({
+        message: "Already joined the community",
+      });
     }
 
+    const community = await prisma.community.update({
+      where: {
+        id: communityId,
+      },
+      data: {
+        members: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    res.json({
+      message: "Joined community successfully",
+      community,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Failed to join community",
+    });
+  }
+});
+
+// leave a community
+router.post("/:communityId/leave", async (req, res) => {
+  const userId = req.user.id;
+  const{communityId} = req.params;
+
+  try{
+    const community = await prisma.community.update({
+      where: {
+        id: communityId,
+      },
+      data:{
+        members:{
+          disconnect:{
+            id:userId
+          }
+        }
+      }
+    })
+    res.json({ message: 'User left the community successfully', community });
+
+  }
+  catch(e){
+    return res.status(500).json({
+      message: "Failed to leave community",
+    });
+  }
 })
 
+//get all communites a user has joined
+router.get("/:userId/joined", async (req, res) => {
+  const { userId } = req.params;
 
+  try{
+    const user = await prisma.user.findUnique({
+      where:{
+        id:userId
+      },
+      include:{
+        communities:true
+      }
+    })
+
+    if(!user){
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.json(user.communities);
+  }
+  catch(e){
+    return res.status(500).json({
+      message: "Failed to get user's joined communities",
+    });
+  }
+})
 
 export default router;
