2aac3af63d59db3dd7bfb45b77421118dae4f2f1
Rish <rishk219@gmail.com>
2025-02-24 18:04:48 +0530

    added typecasting query

--- controller/postController.js
+++ controller/postController.js
@@ -1,140 +1,141 @@
-import { createComment, createPost, getComments, getPosts, likeDislikeComment, likeDislikePost  } from "../services/postService.js";
+import {
+  createComment,
+  createPost,
+  getComments,
+  getPosts,
+  likeDislikeComment,
+  likeDislikePost,
+} from "../services/postService.js";
 
+export async function createPostController(req, res) {
+  const { communityId, content, caption } = req.body;
+  const userId = req.user.id;
+  // 	if (caption){
 
-export async function createPostController(req,res) {
-    const {communityId,content,caption} = req.body;
-    const userId = req.user.id;
-// 	if (caption){
+  // try {
+  //       const response= await fetch('BACKEND_URL', {
+  //         method:"POST",
+  //         headers:{
+  //           "Content-Type": "application/json",
+  //         },
+  //         body:JSON.stringify({
 
+  //           msg:caption,
 
-// try {
-//       const response= await fetch('BACKEND_URL', {
-//         method:"POST",
-//         headers:{
-//           "Content-Type": "application/json", 
-//         },
-//         body:JSON.stringify({
+  //         })});
+  //       const data=await response.json();
 
-//           msg:caption,
+  //       if (response.ok){
 
-//         })});
-//       const data=await response.json();
+  // 	if (data.toxicity){
+  // 	return res.status(500).json({
+  //             message: "Please use Kinder Words"
+  //         })
 
-//       if (response.ok){
-        
-// 	if (data.toxicity){
-// 	return res.status(500).json({
-//             message: "Please use Kinder Words"
-//         })
-	
-// 	}
-//       }else{
-//         throw new Error('some Error Occured');
-//       }
-      
-//     } catch (e) {
-// 	return res.status(500).json({
-//             message: "some Error Occurred"
-//         })
-	
-//     }
-// }
-    
-    try {
-        const post = await createPost(userId,content,caption,communityId)
-        return res.status(201).json({
-            message: "Post created successfully",
-            post,
-            
-        })
+  // 	}
+  //       }else{
+  //         throw new Error('some Error Occured');
+  //       }
 
-    }catch(e){
-        return res.status(500).json({
-            message: "Failed to create post"
-        })
-    }
+  //     } catch (e) {
+  // 	return res.status(500).json({
+  //             message: "some Error Occurred"
+  //         })
+
+  //     }
+  // }
+
+  try {
+    const post = await createPost(userId, content, caption, communityId);
+    return res.status(201).json({
+      message: "Post created successfully",
+      post,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Failed to create post",
+    });
+  }
 }
 
-export async function getPostsController (req, res) {
-
-    const userId = req.user.id;
-    const {skip,take} = req.query;
-    try {
-        const posts = await getPosts(userId,skip,take)
-        return res.status(200).json({
-            posts
-        })
-    }catch(e){
-        return res.status(500).json({
-            message: "Failed to fetch posts why i dont know",
-            err:e,
-            error: e.message
-        })
-    }
+export async function getPostsController(req, res) {
+  const userId = req.user.id;
+  let { skip, take } = req.query;
+  skip = parseInt(skip) || 0;
+  take = parseInt(take) || 15;
+  try {
+    const posts = await getPosts(userId, skip, take);
+    return res.status(200).json({
+      posts,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Failed to fetch posts why i dont know",
+      err: e,
+      error: e.message,
+    });
+  }
 }
 
-export async function likeDislikePostController(req,res) {
+export async function likeDislikePostController(req, res) {
+  const userId = req.user.id;
+  const { postId } = req.body;
 
-    const userId = req.user.id
-    const {postId} = req.body
+  try {
+    const like = await likeDislikePost(userId, postId);
 
-    try {
-        const like = await likeDislikePost(userId , postId)
-
-        return res.status(200).json(
-            like
-        )
-    } catch (error) {
-        return res.status(500).json({
-            error: error.message,
-            message: "Like failed try again later",
-        })
-    }
+    return res.status(200).json(like);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+      message: "Like failed try again later",
+    });
+  }
 }
 
-export async function createCommentController(req,res) {
-    const {content,postId} = req.body
-    const userId = req.user.id
+export async function createCommentController(req, res) {
+  const { content, postId } = req.body;
+  const userId = req.user.id;
 
-    try {
-        const comment = await createComment(userId,postId,content)
-        res.status(200).json({
-            comment
-        })
-    } catch (error) {
-        res.status(500).json({
-            error: error.message,
-            message: "Not been able to comment sir"
-        })
-    }
+  try {
+    const comment = await createComment(userId, postId, content);
+    res.status(200).json({
+      comment,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+      message: "Not been able to comment sir",
+    });
+  }
 }
 
-export async function getCommentController(req,res) {
-    const userId = req.user.id
-    const {postId , skip , take} = req.body;
+export async function getCommentController(req, res) {
+  const userId = req.user.id;
+  const { postId, skip, take } = req.body;
 
-    try {
-        const comments = await getComments(userId,postId,skip,take)
-        res.status(200).json({
-            comments
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: "Unable to fetch comments"
-        })
-    }
+  try {
+    const comments = await getComments(userId, postId, skip, take);
+    res.status(200).json({
+      comments,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Unable to fetch comments",
+    });
+  }
 }
 
-export async function likeDislikeCommentController(req,res) {
-    const userId = req.user.id
-    const {commentId} = req.body;
+export async function likeDislikeCommentController(req, res) {
+  const userId = req.user.id;
+  const { commentId } = req.body;
 
-    try {
-        const like = await likeDislikeComment(userId,commentId)
-        res.status(200).json(like)
-    } catch (error) {
-        res.status(500).json({
-            message: "Liked failed very badly please try again"
-        })
-    }
+  try {
+    const like = await likeDislikeComment(userId, commentId);
+    res.status(200).json(like);
+  } catch (error) {
+    res.status(500).json({
+      message: "Liked failed very badly please try again",
+    });
+  }
 }
--- routes/v1/post/feed/index.js
+++ routes/v1/post/feed/index.js
@@ -1,44 +1,43 @@
 import { Router } from "express";
 
+const router = Router();
 
-const router = Router()
-
-router.get("/" ,async (req,res)=>{
-        const {skip,take} = req.body;
-        try {
-            const posts = await getUnprotectedPost(skip,take)
-            return res.status(200).json({
-                posts
-            })
-        }catch(e){
-            return res.status(500).json({
-                message: "Failed to fetch posts why i dont know",
-                e
-            })
-        }
-    }
-    
-)
-
-async function getUnprotectedPost (skip=0,take=15) {
-  const posts =  await prisma.post.findMany({
-      skip,
-      take,
-      include: {
-        user: {
-          select: {
-            profileImage: true,
-            username: true
-          }
-        }
-      },  
-      orderBy: {
-        createdAt: 'desc', // Sorting baby 
-      },
+router.get("/", async (req, res) => {
+  let { skip, take } = req.query;
+  skip = parseInt(skip) || 0;
+  take = parseInt(take) || 15;
+  try {
+    const posts = await getUnprotectedPost(skip, take);
+    return res.status(200).json({
+      posts,
     });
-    return posts.map((post) => ({
-      ...post,
-      isLiked: false,
-    }))
+  } catch (e) {
+    return res.status(500).json({
+      message: "Failed to fetch posts why i dont know",
+      e,
+    });
+  }
+});
+
+async function getUnprotectedPost(skip = 0, take = 15) {
+  const posts = await prisma.post.findMany({
+    skip,
+    take,
+    include: {
+      user: {
+        select: {
+          profileImage: true,
+          username: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc", // Sorting baby
+    },
+  });
+  return posts.map((post) => ({
+    ...post,
+    isLiked: false,
+  }));
 }
-export default router
+export default router;
