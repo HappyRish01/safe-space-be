18d3914c9cd6a79b5866afd13bbdf14d0b69224a
Rish <rishk219@gmail.com>
2025-02-24 15:06:54 +0530

    redeploying

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import userRoutes from "../routes/v1/user/index.js";
 import postRoutes from "../routes/v1/post/index.js";
 import feedRoutes from "../routes/v1/post/feed/index.js";
 import communityRoutes from "../routes/v1/community/index.js";
-import {getPostsController} from "../controller/postController.js";
 import {generateUploadSignature,saveImage} from "../controller/mediaController.js";
 
 
@@ -15,7 +14,6 @@ const router = Router()
 
 router.use("/v1/user" , userRoutes);
 router.use("/v1/post" ,checkForAuthentication, postRoutes);
-router.use("/v1/posts" ,checkForAuthentication, getPostsController);
 router.use("/v1/feeds" , feedRoutes);
 router.use("/v1/community",checkForAuthentication,communityRoutes)
 router.use("/v1/generate-upload-signature",checkForAuthentication,generateUploadSignature)
