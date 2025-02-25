46d020ef7e1c26fb98e8e91aaebb9a072d5cf853
Rish <107908068+HappyRish01@users.noreply.github.com>
2025-02-25 12:35:39 +0530

    added fetch user details

--- routes/v1/post/index.js
+++ routes/v1/post/index.js
@@ -28,6 +28,30 @@ router.delete("/delete", async (req, res) => {
 });
 
 router.get("/posts", getPostsController);
+router.get("/user" , async (req,res) => {
+    const userId = req.user.id
+    try{
+        const user = await prisma.user.findUnique({
+            where: {
+                id:userId
+            },
+            select: {
+                id: true,
+                username: true,
+                profileImage: true
+                
+            }
+        })
+        return res.status(200).json(user)
+
+    }
+    catch(e){
+        return res.status(500).json({
+            message: "Failed to get user details"
+        })
+    }
+})
+
 
 router.get("/:postId", async (req, res) => {
   const { postId } = req.params;
